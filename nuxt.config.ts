b6beabc50760e9a869e2fb06708e3e3626512fb1
Dusan Cankovic <dusancankovic83@gmail.com>
2025-08-03 13:11:23 +0200

    added favicon to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Product Sign-up',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
